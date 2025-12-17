34e61943eb9e6bfa90bf3278dd7f6e3cd3e0e193
Aidan-Jared <AidanJared42@gmail.com>
2025-12-17 15:52:57 +1300

    data analysis work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.datasets import make_circles, make_moons
+from sklearn.datasets import make_circles, make_moons, make_blobs
 from sklearn.model_selection import StratifiedKFold
 
 from sklearn.metrics import f1_score, accuracy_score
@@ -27,14 +27,15 @@ SEED = 42
 def make_data(n_samples, noise, type = "moons", SEED=42):
     if type == "moons":
         X, y = make_moons(n_samples=n_samples, noise=noise, random_state=SEED, shuffle=True)
+        # X, y = make_blobs(n_samples=n_samples, random_state=SEED, n_features=2, centers=2)
     elif type == "circles":
         X, y = make_circles(n_samples=n_samples, noise=noise, random_state=SEED, shuffle=True)
     else:
         key = jax.random.PRNGKey(SEED)
         key1, key2 = jax.random.split(key)
-        X = jax.random.uniform(key1, (n_samples * 4, 2), minval=-3, maxval=3, dtype=jnp.float32)
+        X = jax.random.uniform(key1, (n_samples, 2), minval=-3, maxval=3, dtype=jnp.float32)
         y = ((jnp.floor(X[:,0]) + jnp.floor(X[:,1])) % 2).astype(int)
-        noise_idx = jax.random.choice(key2, a= n_samples*4, shape = (int(n_samples * 4 * noise),), replace=False)
+        noise_idx = jax.random.choice(key2, a= n_samples, shape = (int(n_samples * noise),), replace=False)
         y = y.at[noise_idx].set(1 - y[noise_idx])
         X = np.array(X)
         y = np.array(y)
@@ -46,7 +47,9 @@ def audit_model(
         y, 
         SEED : int =42, 
         max_features :int = 2,
-        stopping = 5
+        stopping : int = 5,
+        n_splits : int = 5,
+        top_k : int = 5
         ):
     acc_queue = deque(maxlen=stopping)
     acc_queue.extend([0,.1])
@@ -58,7 +61,7 @@ def audit_model(
         model = DecisionTreeClassifier(
             random_state=SEED, max_features=max_features,
             max_depth=d, min_samples_split=2, min_samples_leaf=1)
-        skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=SEED)
+        skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=SEED)
         train_acces = []
         train_f1s = []
         test_acces = []
@@ -82,11 +85,24 @@ def audit_model(
             
 
             attack = DecisionTreeAttack(classifier=classifier)
-            x_attack_adv = attack.generate(x=X_test)
+            try:
+                x_attack_adv = attack.generate(x=X_test)
+            except:
+                break
 
             
             y_adv_pred, acc_adv, f1_adv = eval_model(x_attack_adv, y_test.reshape(y_test.shape[0],1), classifier)
-            adv_distance = np.linalg.norm(X_test - x_attack_adv)
+            distances = []
+            for idx, attack_data in enumerate(x_attack_adv):
+                true_label = y_test[idx]
+                attack_class_points = X_test[y_test != true_label]
+                distance_vector = np.sqrt(np.sum((attack_data - attack_class_points)**2, axis=1))
+                indices = np.argsort(distance_vector)[:top_k]
+                knn_distance = distance_vector[indices]
+                distances.append(np.mean(knn_distance).item())
+
+
+            adv_distance = np.mean(distances)
             train_acces.append(train_acc)
             train_f1s.append(train_f1)
             test_acces.append(test_acc)
@@ -121,18 +137,16 @@ def eval_model(X, y, classifier):
 
 def main():
 
-    seeds = [42,105,89,75,37,8]
+    seeds = [ 42,  125,   58,   86,  138,  137,  146,   37,    5,  179,]
     datatypes = ["moons", "circles", 'check']
 
     res = pl.DataFrame()
     
     for i in datatypes:
         for j in seeds:
-    # plt.scatter(X[:, 0], X[:, 1], c=y, cmap='viridis')
-    # plt.show()
             print(i + f"_{j}")
-            X, y = make_data(1000, .1, i)
-            results = audit_model(X, y,SEED=j)
+            X, y = make_data(5000, .15, i)
+            results = audit_model(X, y,SEED=j, n_splits=10)
 
             results = results.with_columns(pl.lit(i).alias("distribution"), pl.lit(j).alias("seed"))
 
